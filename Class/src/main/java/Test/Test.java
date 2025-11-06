03ee31c6a27cd5d0c315c5da075c2e24d6f25d73
Ayad69 <88985906+Ayad69@users.noreply.github.com>
2025-11-06 10:46:56 +0100

    add class test and test the dto

---
+++ Class/src/main/java/Test/Test.java
@@ -0,0 +1,20 @@
+package Test;
+
+import src.DTO.Classe;
+import src.DTO.Eleve;
+import src.DTO.Prof;
+
+import java.util.ArrayList;
+
+public class Test {
+    public static void main(String[] args) {
+        Eleve eleve = new Eleve("Hassani","Ayad",20);
+        Eleve eleve2 = new Eleve("Geek","Leo",20);
+        Prof prof = new Prof("Plantefeve","Romain",45);
+        ArrayList<Eleve> eleves = new ArrayList<Eleve>();
+        eleves.add(eleve);
+        eleves.add(eleve2);
+        Classe classe = new Classe("3eme",prof,eleves);
+        System.out.println(classe.toString());
+    }
+}
